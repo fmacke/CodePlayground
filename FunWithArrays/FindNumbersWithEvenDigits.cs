6c76b2128ee64422aef0fc2e58f900583dd2feb2
Finn Mackenzie <finn@pikupstix.co.uk>
2025-03-10 10:12:20 +0000

    Add FindNumbersWithEvenDigits solver.

---
+++ FunWithArrays/FindNumbersWithEvenDigits.cs
@@ -0,0 +1,31 @@
+﻿namespace CodePlayground.FunWithArrays
+{
+    /// <summary>
+    /// This class provides a solution to find the count of numbers with an even number of digits in a given array.
+    /// </summary>
+    public class FindNumbersWithEvenDigits : ISolution
+    {
+        private int[] nums;
+        public FindNumbersWithEvenDigits(int[] ints)
+        {
+            this.nums = ints;
+        }
+
+        /// <summary>
+        /// Solves the problem by counting the numbers with an even number of digits in the array.
+        /// </summary>
+        /// <returns>The count of numbers with an even number of digits.</returns>
+        public int Solve()
+        {
+            int count = 0;
+            foreach (var num in nums)
+            {
+                if (num.ToString().Length % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
